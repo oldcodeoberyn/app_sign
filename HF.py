de0a5dabc349c71d9c5f49d7d0c77665d68a2ed1
lij021 <lij021@foresealife.com>
2018-07-04 20:20:55 +0800

    not_commit

--- HF.py
+++ HF.py
@@ -45,6 +45,10 @@ def login(userName, password):
         time.sleep(2)
 
         date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+        buy_time_str = date + ' 09:59:59'
+        buytime_10 = time.mktime(time.strptime(buy_time_str, "%Y-%m-%d %H:%M:%S"))
+
         buy_time_str = date + ' 11:59:59'
         buytime_12 = time.mktime(time.strptime(buy_time_str, "%Y-%m-%d %H:%M:%S"))
 
@@ -72,7 +76,9 @@ def login(userName, password):
                    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
                    "cookie":cookie
                    }
-        while time.time() < buytime + 0.95:
+        while time.time() < buytime + 0.8:
+            time.sleep(0.1)
+        while time.time() > buytime + 0.8 and time.time() < buytime:
             time.sleep(0.025)
             resp = requests.get(
                 "http://act-jshop.jd.com/couponSend.html?ruleId=12816307&key=9e2c507aa97c43f9a9dad267bbd4bb40&sid=df194dd9015d61b069c80548c75a8527&eid=63MC7EDDUH3243MORQ6DB7M4DE5MRVJBHY45TJTOLUBLHKPV7ZM5K7NODMX23BJDXO5D3PO4WJDNP3CLSH23KQ4LHY&fp=a5cfd575a431a5c829b88a9062947ac4&shshshfp=a5b45026da1ec74832d172380fe4983e&shshshfpa=aa869845-629a-d170-0163-904170f09f1d-1530679125&shshshfpb=0dc613037052c5423615861c74482464d9908091518dc06c55b3446a37&jda=122270672.15301079406641749654337.1530107941.1530678243.1530684086.21&pageClickKey=-1&platform=3&applicationId=983467&_=1530684192499&callback=Zepto1530684165390",headers=headers)
@@ -80,6 +86,9 @@ def login(userName, password):
 
         resp = requests.get(
             "http://act-jshop.jd.com/couponSend.html?ruleId=12816307&key=9e2c507aa97c43f9a9dad267bbd4bb40&sid=df194dd9015d61b069c80548c75a8527&eid=63MC7EDDUH3243MORQ6DB7M4DE5MRVJBHY45TJTOLUBLHKPV7ZM5K7NODMX23BJDXO5D3PO4WJDNP3CLSH23KQ4LHY&fp=a5cfd575a431a5c829b88a9062947ac4&shshshfp=a5b45026da1ec74832d172380fe4983e&shshshfpa=aa869845-629a-d170-0163-904170f09f1d-1530679125&shshshfpb=0dc613037052c5423615861c74482464d9908091518dc06c55b3446a37&jda=122270672.15301079406641749654337.1530107941.1530678243.1530684086.21&pageClickKey=-1&platform=3&applicationId=983467&_=1530684192499&callback=Zepto1530684165390",headers=headers)
+        logger.debug(resp.text)
+        resp = requests.get(
+            "http://act-jshop.jd.com/couponSend.html?ruleId=12816308&key=9e2c507aa97c43f9a9dad267bbd4bb40&sid=df194dd9015d61b069c80548c75a8527&eid=63MC7EDDUH3243MORQ6DB7M4DE5MRVJBHY45TJTOLUBLHKPV7ZM5K7NODMX23BJDXO5D3PO4WJDNP3CLSH23KQ4LHY&fp=a5cfd575a431a5c829b88a9062947ac4&shshshfp=a5b45026da1ec74832d172380fe4983e&shshshfpa=aa869845-629a-d170-0163-904170f09f1d-1530679125&shshshfpb=0dc613037052c5423615861c74482464d9908091518dc06c55b3446a37&jda=122270672.15301079406641749654337.1530107941.1530678243.1530684086.21&pageClickKey=-1&platform=3&applicationId=983467&_=1530684192499&callback=Zepto1530684165390",headers=headers)
         logger.debug(resp.text)
         driver.get("https://sale.jd.com/m/act/8Ak3ilHTvYR7.html")
         time.sleep(0.3)
